c8bf855dce413ca2a3039585f5bee1cfbe7374b8
Thanh Long <mystorage20119@gmail.com>
2019-06-28 16:26:47 +0700

    fix compile

--- android/libraries/rib-android/src/main/java/com/uber/rib/core/IBuilder.kt
+++ android/libraries/rib-android/src/main/java/com/uber/rib/core/IBuilder.kt
@@ -2,6 +2,6 @@ package com.uber.rib.core
 
 import dagger.android.DispatchingAndroidInjector
 
-interface IBuilder {
-    fun build(injector: DispatchingAndroidInjector<Any>)
+interface IBuilder<T: Router<*>> {
+    fun build(injector: DispatchingAndroidInjector<Any>): T
 }
